ea1eb382bd9204ef68f92661e959a7ba2ba12963
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-01-11 10:15:48 +0900

    Modify BOJ 1406

--- BOJ/백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/200(자료구조 1)/1406(에디터).cpp
+++ BOJ/백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/200(자료구조 1)/1406(에디터).cpp
@@ -1,28 +1,60 @@
 #include <iostream>
 #include <string>
+#include <stack>
 using namespace std;
 
-template <typename T>
-class List{
-    struct node{
-        T data;
-        node *prev,*next;
-    };
-    using link=node*;
+int n;
+//각각 커서의 왼쪽과 오른쪽에 있는 글자들을 저장할 스택
+stack<char> left_stk,right_stk;
 
-    link head,cur,tail;
-    //head와 tail은 더미노드
-    //cur는 head를 제외한 모든 노드를 가리킬 수 있다.
+void input(){
+    string str;
 
-public:
-    List();
-    void Left();
-    void Right();
-    void Add(T x);
-    void Delete();
-    void Display();
-    ~List();
-};
+    cin>>str;
+
+    for(char c:str){
+        left_stk.push(c);
+    }
+}
+
+void L(){
+    if(!left_stk.empty()){
+        right_stk.push(left_stk.top());
+        left_stk.pop();
+    }
+
+    return;
+}
+
+void D(){
+    if(!right_stk.empty()){
+        left_stk.push(right_stk.top());
+        right_stk.pop();
+    }
+
+    return;
+}
+
+void B(){
+    if(!left_stk.empty()){
+        left_stk.pop();
+    }
+}
+
+void P(char c){
+    left_stk.push(c);
+}
+
+void output(){
+    while(!left_stk.empty()){
+        L();
+    }
+
+    while(!right_stk.empty()){
+        cout<<right_stk.top();
+        right_stk.pop();
+    }
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -30,111 +62,37 @@ int main(){
     cout.tie(NULL);
     //c++의 표준 stream의 동기화를 끊는 역할을 하여 입출력의 속도를 높인다.
 
-    string str;
-    cin>>str;
+    input();
 
-    List<char> editor;
-
-    for(int i=0;i<str.length();i++){
-        editor.Add(str[i]);
-    }
-
-
-    int n;
     cin>>n;
 
-    for(int i=0;i<n;i++){
-        char m,tmp;
-        cin>>m;
+    for(int i=0;i<n;++i){
+        char command;
+        char c;
 
-        switch(m){
+        cin>>command;
+
+        switch(command){
             case 'L':
-                editor.Left();
+                L();
                 break;
+
             case 'D':
-                editor.Right();
+                D();
                 break;
+
             case 'B':
-                editor.Delete();
+                B();
                 break;
+
             case 'P':
-                cin>>tmp;
-                editor.Add(tmp);
+                cin>>c;
+                P(c);
                 break;
         }
     }
 
-    editor.Display();
+    output();
 
     return 0;
-}
-
-template <typename T>
-List<T>::List():head(new node),tail(new node){
-    head->prev=NULL;
-    head->next=tail;
-
-    tail->prev=head;
-    tail->next=NULL;
-
-    cur=tail;
-}
-
-template <typename T>
-void List<T>::Left(){
-    if(cur->prev!=head){
-        cur=cur->prev;
-    }
-}
-
-template <typename T>
-void List<T>::Right(){
-    if(cur->next!=NULL){
-        cur=cur->next;
-    }
-}
-
-template <typename T>
-void List<T>::Add(T x){
-    link tmp=new node;
-    tmp->data=x;
-
-    cur->prev->next=tmp;
-    tmp->prev=cur->prev;
-
-    cur->prev=tmp;
-    tmp->next=cur;
-}
-
-template <typename T>
-void List<T>::Delete(){
-    if(cur->prev!=head){
-        link tmp=cur->prev;
-        tmp->prev->next=cur;
-        cur->prev=tmp->prev;
-        delete tmp;
-    }
-}
-
-template <typename T>
-void List<T>::Display(){
-    link tmp=head->next;
-
-    while(tmp!=tail){
-        cout<<tmp->data;
-        tmp=tmp->next;
-    }
-    cout<<'\n';
-}
-
-template <typename T>
-List<T>::~List(){
-    cur=NULL;
-    tail=NULL;
-
-    while(head){
-        link tmp=head;
-        head=head->next;
-        delete tmp;
-    }
 }
