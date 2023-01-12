309ff8ad7abbb627a7712598d1dad6023d2472ac
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-01-12 10:38:46 +0900

    Modify BOJ 1158
    Change code with C++ STL

--- BOJ/백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/200(자료구조 1)/1158(요세푸스 문제).cpp
+++ BOJ/백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/200(자료구조 1)/1158(요세푸스 문제).cpp
@@ -1,145 +1,47 @@
 #include <iostream>
-#include <string>
+#include <queue>
 using namespace std;
 
-class CDLL{
-    struct node{
-        int num;
-        node *prev,*next;
-    };
-    using link=node*;
+int n,k;
 
-    link head;
+void Josephus(){
+    queue<int> ring;
+    //순서
+    int order=1;
 
-public:
-    CDLL();
-    void Add(int x);
-    void Delete(int x);
-    int Empty();
-    void Josephus(int n,int k);
-    void Display();
-    ~CDLL();
-};
-
-int main(){
-    int n,k;
-    cin>>n>>k;
-
-    CDLL ring;
-
-    for(int i=1;i<=n;i++){
-        ring.Add(i);
+    //1부터 n까지 큐에 넣기
+    for(int i=1;i<=n;++i){
+        ring.push(i);
     }
 
-    ring.Josephus(n,k);
+    cout<<'<';
+    while(!ring.empty()){
+        //큐의 가장 앞 원소를 임시 변수에 저장
+        int tmp=ring.front();
 
-    return 0;
-}
+        ring.pop();
 
-CDLL::CDLL():head(new node){
-    head->prev=head;
-    head->next=head;
-}
-
-void CDLL::Add(int x){
-    link tmp=new node;
-    tmp->num=x;
-
-    if(Empty()){
-        tmp->next=tmp;
-        tmp->prev=tmp;
-
-        head->next=tmp;
-    }
-    else{
-        tmp->prev=head->next->prev;
-        tmp->next=head->next;
-
-        head->next->prev->next=tmp;
-        head->next->prev=tmp;
-    }
-}
-
-void CDLL::Delete(int x){
-    if(Empty()){
-        return;
-    }
-
-    link i=head->next;
-    do{
-        if(i->num==x){
-            if(i==head->next){
-                if(head->next->next==head->next){
-                    head->next=head;
-                }
-                else{
-                    head->next=i->next;
-                }
-            }
-            i->prev->next=i->next;
-            i->next->prev=i->prev;
-            delete i;
-            return;
-        }
-
-        i=i->next;
-    }while(i!=head->next);
-
-}
-
-int CDLL::Empty(){
-    if(head->next==head){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-
-}
-
-void CDLL::Josephus(int n,int k){
-    int order=0;
-    link cur=head;
-
-    cout<<"<";
-
-    while(!Empty()){
-        cur=cur->next;
-        order+=1;
-
-        if(order==k){
-            order=0;
-            cur=cur->prev;
-            cout<<cur->next->num;
-            Delete(cur->next->num);
-            if(Empty()){
-                cout<<">";
-            }
-            else{
+        //k번째 번호 제거 후 출력
+        if(order%k==0){
+            cout<<tmp;
+            if(!ring.empty()){
                 cout<<", ";
             }
         }
+        else{
+            ring.push(tmp);
+        }
 
+        order++;
     }
-    cout<<endl;
+
+    cout<<'>';
 }
 
-void CDLL::Display(){
-    link tmp=head->next;
+int main(){
+    cin>>n>>k;
 
-    do{
-        cout<<tmp->num<<" ";
-        tmp=tmp->next;
-    }while(tmp!=head->next);
-    cout<<endl;
-}
+    Josephus();
 
-CDLL::~CDLL(){
-    head->next->prev->next=NULL;
-
-    while(head){
-        link tmp=head;
-        head=head->next;
-        delete tmp;
-    }
+    return 0;
 }
